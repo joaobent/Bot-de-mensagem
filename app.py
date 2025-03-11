decf34340065aaecd3b7468db4a7a40f9ddaa442
joaobent <joaopedro0744@hotmail.com>
2025-03-10 21:54:52 -0400

    melhorei um pouco o codigo

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import pyautogui
 workbook = openpyxl.load_workbook('clientes.xlsx')
 pagina_clientes = workbook['Sheet1']
 
+webbrowser.open('https://web.whatsapp.com')
+sleep(7)
+
 for linha in pagina_clientes.iter_rows(min_row=2):
     nome = linha[0].value
     telefone = linha[1].value
